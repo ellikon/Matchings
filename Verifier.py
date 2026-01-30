786303b13f4245788fae0a84d56ab31015ac613e
ordinalsoul <55070893+ordinalsoul@users.noreply.github.com>
2026-01-30 13:02:03 -0500

    verifier checks student and hospital preferences

--- Verifier.py
+++ Verifier.py
@@ -19,4 +19,21 @@ def read_intstance(path):
         if n < 0:
             raise ValueError("INVALID INPUT: n must be a non negative integer")
 
+        # Hospital preferences
+        for i in range(n):
+            line = file.readline()
+            if line == "":
+                raise ValueError(f"INVALID INPUT: missing hospital preference in line {i + 2}")
+            hospitalPreferences.append(list(map(int, line.split())))
+
+        #Student preferences
+        for i in range(n):
+            if line == "":
+                raise ValueError(f"INVALID INPUT: missing hospital preference in line {i+n+2}")
+            studentPreferences.append(list(map(int, line.split())))
+
+        #Remainder
+        rest = file.read().strip()
+        if rest != "":
+            raise ValueError("INVALID INPUT: extra data after hospital and student preferences")
 
