c7a4b5bc886da7c4a9a5239d3f5b40b64a71ef5c
ellikon <robbie@millertribe.com>
2026-01-29 22:55:37 -0500

    Added data input and parsing

--- Matcher.py
+++ Matcher.py
@@ -1,1 +1,9 @@
+hospitalPreferences = []
+studentPreferences = []
 
+with open('data/example.in', 'r') as file:
+    n = int(file.readline().strip())
+    for i in range(n):
+        hospitalPreferences.append(list(map(int, file.readline().split())))
+    for i in range(n):
+        studentPreferences.append(list(map(int, file.readline().split())))
