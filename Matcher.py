b4e31e20b74ea7d5bee1b131893ba585fa9a130e
ellikon <robbie@millertribe.com>
2026-01-29 23:26:40 -0500

    Added function to find h prime and validate a student swap

--- Matcher.py
+++ Matcher.py
@@ -29,3 +29,30 @@ def find_next_student(h):
         if [h, s] not in previousMatches:
             return s
     return -1
+
+
+def get_h_prime(s):
+    h_prime = -1
+    for match in matches:
+        if matches[match] == s:
+            h_prime = match
+    return h_prime
+
+
+def check_student_swap(s, h):
+    h_prime = get_h_prime(s)
+    if h_prime == -1:
+        return True
+
+    return studentPreferences[s].index(h) > studentPreferences[h].index(h_prime)
+
+# while find_unmatched_hospital() != -1:
+#     hospital = find_unmatched_hospital()
+#     hospitalPreference = hospitalPreferences[hospital-1]
+#
+#     student = find_next_student(hospital)
+#     studentPreferences[student] = studentPreferences[student-1]
+#
+#     if student not in matches.values():
+#         matches[hospital] = student
+#     else if
