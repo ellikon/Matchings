3def4f1c2d107deac0f4d1937666658726819a98
ellikon <robbie@millertribe.com>
2026-01-29 23:04:17 -0500

    Added function for finding the first unmatched hospital

--- Matcher.py
+++ Matcher.py
@@ -7,3 +7,15 @@ with open('data/example.in', 'r') as file:
         hospitalPreferences.append(list(map(int, file.readline().split())))
     for i in range(n):
         studentPreferences.append(list(map(int, file.readline().split())))
+
+matches = {}
+
+for i in range(n):
+    matches[i + 1] = -1
+
+
+def find_unmatched_hospital():
+    for hospital in matches:
+        if matches[hospital] == -1:
+            return hospital
+    return -1
